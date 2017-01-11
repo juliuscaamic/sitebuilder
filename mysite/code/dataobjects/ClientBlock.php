702cf1a224acab0cfabbf0e2f515410490d6b163
Julius Caamic <julius@greenbrainer.com>
2017-01-11 17:37:22 +0800

    Fix the build bug due to the clients block

--- mysite/code/dataobjects/ClientBlock.php
+++ mysite/code/dataobjects/ClientBlock.php
@@ -1,9 +1,7 @@
 <?php
 class ClientBlock extends TextBlock {
 	
-	private static $db = array(
-		'Clients' => 'Client'
-	);
+
 
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
